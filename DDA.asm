c8c13e0fcddbe5908ab866835e0a3a52e05e97aa
GeeTwentyFive <59966523+GeeTwentyFive@users.noreply.github.com>
2024-08-05 18:38:28 +0200

    Optimizations

--- DDA.asm
+++ DDA.asm
@@ -3,11 +3,12 @@
 ; xmm2 == x2 (rounded to whole number)
 ; xmm3 == y2 (rounded to whole number)
 draw_line:
-	sub rsp, 0x40
-	movups [rsp], xmm9
-	movups [rsp+0x10], xmm8
-	movups [rsp+0x20], xmm7
-	movups [rsp+0x30], xmm6
+	sub rsp, 0x50
+	movups [rsp], xmm10
+	movups [rsp+0x10], xmm9
+	movups [rsp+0x20], xmm8
+	movups [rsp+0x30], xmm7
+	movups [rsp+0x40], xmm6
 
 	subss xmm2, xmm0 ; dx1 = x2 - x1
 	subss xmm3, xmm1 ; dy1 = y2 - y1
@@ -40,19 +41,22 @@ draw_line:
 	mov eax, __float32__(4.0)
 	movd xmm7, eax ; xmm7 = 4.0 f32
 
+	cvtsi2ss xmm8, [PIXELS_PER_SCAN_LINE]
+
+	mov rcx, [FRAMEBUFFER_BASE_PTR]
+
 .loop_draw_line:
-	movaps xmm8, xmm0 ; x1_2 -> xmm8
-	mulss xmm8, xmm7 ; x = x1_2 * 4.0 f32
+	movaps xmm9, xmm0 ; x1_2 -> xmm9
+	mulss xmm9, xmm7 ; x = x1_2 * 4.0 f32
 
-	cvtsi2ss xmm9, [PIXELS_PER_SCAN_LINE]
-	mulss xmm9, xmm1 ; y = PIXELS_PER_SCAN_LINE f32 * y1
-	mulss xmm9, xmm7 ; y = y * 4.0 f32
+	movaps xmm10, xmm1 ; y1_2 -> xmm10
+	mulss xmm10, xmm8 ; y = PIXELS_PER_SCAN_LINE f32 * y1
+	mulss xmm10, xmm7 ; y = y * 4.0 f32
 
-	addss xmm8, xmm9 ; xy = x + y
+	addss xmm9, xmm10 ; xy = x + y
 
-	cvttss2si eax, xmm8 ; int(xy) -> eax
-	mov ecx, [FRAMEBUFFER_BASE_PTR]
-	mov dword [abs ecx+eax], FG_COLOR_ARGB
+	cvttss2si eax, xmm9 ; int(xy) -> eax
+	mov dword [abs rcx+rax], FG_COLOR_ARGB
 
 	addss xmm0, xmm2 ; x1 = x1 + dx
 	addss xmm1, xmm3 ; y1 = y1 + dy
@@ -63,9 +67,11 @@ draw_line:
 
 
 
-	movups xmm6, [rsp+0x30]
-	movups xmm7, [rsp+0x20]
-	movups xmm8, [rsp+0x10]
-	movups xmm9, [rsp]
-	add rsp, 0x40
+	movups xmm6, [rsp+0x40]
+	movups xmm7, [rsp+0x30]
+	movups xmm8, [rsp+0x20]
+	movups xmm9, [rsp+0x10]
+	movups xmm10, [rsp]
+	add rsp, 0x50
 	ret
+
